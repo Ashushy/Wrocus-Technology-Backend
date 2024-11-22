b064e5e806900ef905a7f43a6d93bb51d4f028de
Rahul Yadav <rahulyadav@Rahuls-MacBook-Air.local>
2024-11-22 14:26:48 +0530

    path change for file uplaod

--- utils/uploads.js
+++ utils/uploads.js
@@ -4,7 +4,7 @@ const path = require('path');
 
  
 const storage = multer.diskStorage({
-    destination: './uploads',
+    destination: './tmp/uploads',
     filename: function(req, file, cb) {
       cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
     }
